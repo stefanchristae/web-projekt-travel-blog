d2c6994ea3086eb9633c266665cad6fdf5bf8768
Stefan.Christen <stefan.christen@stud.hslu.ch>
2019-09-06 14:51:16 +0200

    add frontend httpclient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { BlogsComponent } from './blogs/blogs.component';
 import { BlogDetailComponent } from './blog-detail/blog-detail.component';
+import { HttpClientModule }    from '@angular/common/http';
 
 
 @NgModule({
@@ -19,6 +20,7 @@ import { BlogDetailComponent } from './blog-detail/blog-detail.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    HttpClientModule,
     NgbModule // bootstrap
   ],
   providers: [],
--- src/app/blog.service.ts
+++ src/app/blog.service.ts
@@ -1,20 +1,101 @@
 import { Injectable } from '@angular/core';
 import { Blog } from './blog';
-import { MOCK_BLOGS } from './mock-blog';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogService {
+  private blogsUrl = '/blogs';  // URL to web api
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+  ) { }
 
   getBlogs(): Observable<Blog[]> {
-    return of(MOCK_BLOGS);
+    return this.http.get<Blog[]>(this.blogsUrl).pipe(
+      tap(_ => this.log('fetched blogs')),
+      catchError(this.handleError<Blog[]>('getBlogs', [])));
   }
 
-  getBlog(id: number):  Observable<Blog> { 
-    return of(MOCK_BLOGS.find(blog => blog.id === id));
+  getBlog(id: number): Observable<Blog> {
+    const url = `${this.blogsUrl}/${id}`;
+    return this.http.get<Blog>(url).pipe(
+      tap(_ => this.log(`fetched blog id=${id}`)),
+      catchError(this.handleError<Blog>(`getBlog id=${id}`))
+    );
+  }
+
+  //////// Save methods //////////
+
+  /** POST: add a new blog to the server */
+  addBlog(blog: Blog): Observable<Blog> {
+    return this.http.post<Blog>(this.blogsUrl, blog, this.httpOptions).pipe(
+      tap((newBlog: Blog) => this.log(`added blog w/ id=${newBlog.id}`)),
+      catchError(this.handleError<Blog>('addBlog'))
+    );
+  }
+
+  /** DELETE: delete the hero from the server */
+  deleteHero(blog: Blog | number): Observable<Blog> {
+    const id = typeof blog === 'number' ? blog : blog.id;
+    const url = `${this.blogsUrl}/${id}`;
+
+    return this.http.delete<Blog>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`deleted blog id=${id}`)),
+      catchError(this.handleError<Blog>('deleteBlog'))
+    );
+  }
+
+  /** PUT: update the blog on the server */
+  updateBlog(blog: Blog): Observable<any> {
+    return this.http.put(this.blogsUrl, blog, this.httpOptions).pipe(
+      tap(_ => this.log(`updated blog id=${blog.id}`)),
+      catchError(this.handleError<any>('updateBlog'))
+    );
+  }
+
+  /** GET hero by id. Return `undefined` when id not found */
+  getBlogNo404<Data>(id: number): Observable<Blog> {
+    const url = `${this.blogsUrl}/?id=${id}`;
+    return this.http.get<Blog[]>(url)
+      .pipe(
+        map(blogs => blogs[0]), // returns a {0|1} element array
+        tap(h => {
+          const outcome = h ? `fetched` : `did not find`;
+          this.log(`${outcome} blo id=${id}`);
+        }),
+        catchError(this.handleError<Blog>(`getBlog id=${id}`))
+      );
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    console.log(`BlogService: ${message}`);
+  }
+
+  /**
+ * Handle Http operation that failed.
+ * Let the app continue.
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
+ */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
